e8385fecac701f33d74a8085e5ef47d10bd4512a
Leon <lowpolygonum@gmail.com>
2016-06-14 23:00:00 +0200

    game of life with shader

---
+++ Assets/GameOfLife/Scripts/FrameBuffer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrameBuffer
+{
+	RenderTexture[] textures;
+	int currentTexture;
+
+	public FrameBuffer ()
+	{
+		currentTexture = 0;
+		textures = new RenderTexture[2];
+	}
+
+	public void Create (int width, int height)
+	{
+		for (int i = 0; i < textures.Length; ++i) 
+		{
+			if (textures[i]) 
+			{
+				textures[i].Release();
+			}
+			textures[i] = new RenderTexture(width, height, 24, RenderTextureFormat.ARGB32);
+			textures[i].Create();
+			// textures[i].filterMode = FilterMode.Point;
+		}
+	}
+
+	public void Swap ()
+	{
+		currentTexture = (currentTexture + 1) % 2;
+	}
+
+	public RenderTexture Get ()
+	{
+		return textures[currentTexture];
+	}
+}
