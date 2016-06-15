de3fa034b9ed4ab703d387f079568d4e29d8bf42
Leon Denise <leon.denise@free.fr>
2016-06-15 16:59:27 +0200

    let me commit this

---
+++ Assets/Scaling.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scaling : MonoBehaviour {
+
+	public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 scale = transform.localScale;
+		scale.y = 0.5f + Mathf.Sin(Time.time * speed) * 0.5f;
+		transform.localScale = scale;
+	}
+}
---
+++ Assets/Target.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour {
+
+	public Material material;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		material.SetVector("_Target", transform.position);
+	}
+}
